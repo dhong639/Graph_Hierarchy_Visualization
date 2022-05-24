0b16980346f4d463ce924b66bcc2aa76bc2cb26c
dhong639 <30446369+dhong639@users.noreply.github.com>
2022-05-24 15:27:49 -0500

    Update Network.js

--- Network.js
+++ Network.js
@@ -4,6 +4,17 @@ class Network {
 		for(var i = 0; i<count_sites; i++) {
 			var color = this.get_colorRandom()
 			var invert = this.get_colorInvert(color)
+			/**
+			 * try to put light text against dark background
+			 * readability results may vary
+			 */
+			if(color > invert) {
+				var temp = invert
+				invert = color
+				color = temp
+			}
+			color = this.adjust_color(color, -50)
+			invert = this.adjust_color(invert, 50)
 			var nodes = Math.floor(Math.random() * count_nodes) + 1
 			var ports = Math.floor(Math.random() * count_ports) + 1
 			this.dict_site[color.substring(1)] = {
@@ -12,6 +23,10 @@ class Network {
 				data: new Graph(nodes, ports)
 			}
 		}
+		/*for(var site_id in this.dict_site){
+			var root = this.dict_site[site_id].data.root
+			console.log(this.dict_site[site_id].data.breadth_first(root))
+		}*/
 		this.cy = cytoscape({
 			container: document.getElementById(div_id),
 			//wheelSensitivity: 0.1,
@@ -38,13 +53,41 @@ class Network {
 					}
 				},
 				{
-					selector: ':selected',
+					selector: 'node:selected',
 					style: {
 						'background-color': 'data(invert)',
 						'color': 'data(color)',
 					}
 				}
 			]
+		})
+		this.cy.on('tap', 'edge', (edge) => {
+			var edge_id = edge.target.id()
+			console.log(edge_id)
+			var source = edge_id.split('_')[0]
+			var target = edge_id.split('_')[1]
+			var source_site = source.split('.')[0]
+			var source_id = source.split('.')[1]
+			var target_site = target.split('.')[0]
+			var target_id = target.split('.')[1]
+			if(source_id) {
+				var list_edge = this.dict_site[target_site].data.breadth_first(target_id, true)
+				list_edge.reverse()
+				if(list_edge.length != 0) {
+					list_edge.forEach(edge => {
+						var child_id = target_site + '.' + edge[1]
+						var parent_id = target_site + '.' + edge[0]
+						var edge_id = parent_id + '_' + child_id
+						this.cy.remove('#' + edge_id)
+						this.cy.remove('#' + child_id)
+						this.cy.remove('#' + parent_id)
+					})
+				}
+				else {
+					this.cy.remove('#' + edge_id)
+					this.cy.remove('#' + target)
+				}
+			}
 		})
 	}
 	display() {
@@ -103,7 +146,7 @@ class Network {
 		}).run();
 	}
 	get_id_node(site_id, id) {
-		return id == 'S' ? 'S' : site_id + '-' + id
+		return id == 'S' ? 'S' : site_id + '.' + id
 	}
 	get_id_edge(site_id, source, target) {
 		var source_id = this.get_id_node(site_id, source)
@@ -149,5 +192,41 @@ class Network {
 		color = ("000000" + color).slice(-6); // pad with leading zeros
 		color = "#" + color; // prepend #
 		return color;
+	}
+	adjust_color(col, amt) {
+		var usePound = false;
+		var final_length = 6
+		if(col[0] == "#") {
+			col = col.slice(1);
+			usePound = true;
+			final_length = 7
+		}
+		var num = parseInt(col,16);
+		var r = (num >> 16) + amt;
+		if(r > 255){
+			r = 255
+		}
+		else if(r < 0){
+			r = 0
+		}
+		var b = ((num >> 8) & 0x00FF) + amt;
+		if(b > 255){
+			b = 255
+		}
+		else if(b < 0){
+			b = 0
+		}
+		var g = (num & 0x0000FF) + amt;
+		if(g > 255){
+			g = 255
+		}
+		else if(g < 0){
+			g = 0
+		}
+		var string = (usePound?"#":"") + (g | (b << 8) | (r << 16)).toString(16)
+		while(string.length < final_length) {
+			string += '0'
+		}
+		return string
 	}
 }
